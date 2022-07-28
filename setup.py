40231508a8f1d45bbc84ad371847813b54daa097
Ana <ana.sampa.oi@gmail.com>
2022-07-28 12:19:09 -0300

    Add __init__.py

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
 from setuptools import setup, find_packages
 
-VERSION = 0.0.1
-DESCRIPTION = "Metaembedding tool."
-LONG_DESCRIPTION = "A tool to combine and visualize embedding combinations for similarity prediction."
-
-setup(name='metaembedding',
-      version=VERSION,
-      description=DESCRIPTION,
-      long_description = LONG_DESCRIPTION,
-      url='http://github.com/anasampa/metaembedding',
+setup(name='meta',
+      version='0.2.0.1',
+      description='Local Interpretable Model-Agnostic Explanations for machine learning classifiers',
+      url='http://github.com/marcotcr/metaembedding',
       author='Ana',
-      author_email='ana.sampa.oi@gmail.com',
-      license='MIT',
-      packages=find_packages(),
-      python_requires='>=3.7',
+      author_email='marcotcr@gmail.com',
+      license='BSD',
+      packages=find_packages(exclude=['js', 'node_modules', 'tests']),
+      python_requires='>=3.5',
       install_requires=[
-          'matplotlib'],
-          #'numpy',
-          #'sentence_transformers',
-          #'tensorflow.keras',
-          #'tensorflow_probability'
-      #],
+          'matplotlib',
+          'numpy',
+          'scipy',
+          'tqdm >= 4.29.1',
+          'scikit-learn>=0.18',
+          'scikit-image>=0.12',
+          'pyDOE2==1.3.0'
+      ],
+      extras_require={
+          'dev': ['pytest', 'flake8'],
+      },
       include_package_data=True,
       zip_safe=False)
