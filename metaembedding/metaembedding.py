53e0dda761dc17721b8f9fb44fd61adfa04d1261
anasampa <ana.sampa.oi@gmail.com>
2022-08-01 10:37:08 -0300

    Add validation data error and validation run inside train_run.

--- metaembedding/metaembedding.py
+++ metaembedding/metaembedding.py
@@ -122,16 +122,19 @@ class MetaEmbedding():
 
     return X_train_emb
 
-  def train_run(self, X_train,Y_train, x_val=False, y_val=False, epoch=1, shuffle=True):
+  def train_run(self, X_train,Y_train, x_val=False, y_val=False, epochs=1, shuffle=True):
 
-    if not x_val:
-      X_train, x_val, Y_train, y_val = train_test_split(X_train, Y_train, test_size=0.15, shuffle=True)
+    if not x_val and not y_val:
+      self.model.fit(X_train, Y_train, epochs=epochs, shuffle=shuffle,callbacks=[callback])
+    elif x_val and y_val:
+      #X_train, x_val, Y_train, y_val = train_test_split(X_train, Y_train, test_size=0.15, shuffle=True)
+      x_train_emb = self.get_models_emb(X_train)
+      x_val_emb = self.get_models_emb(x_val)
 
-    x_train_emb = self.get_models_emb(X_train)
-    x_val_emb = self.get_models_emb(x_val)
-
-    callback = EarlyStopping(monitor='loss', patience=2)
-    self.model.fit(x_train_emb, Y_train, epochs=2, shuffle=shuffle,validation_data=(x_val_emb, y_val),callbacks=[callback])
+      callback = EarlyStopping(monitor='loss', patience=2)
+      self.model.fit(x_train_emb, Y_train, epochs=, shuffle=shuffle,validation_data=(x_val_emb, y_val),callbacks=[callback])
+    else:
+        raise TypeError("Validation input error. Missing x_val or y_val.")
     return self.model
 
   def predict_model(self,X_test):
