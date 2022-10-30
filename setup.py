5ad061487522330d0c8120407a32051bab465493
anasampa <36799215+anasampa@users.noreply.github.com>
2022-10-30 11:23:19 -0300

    Teste alterando branch para master do lime.

--- setup.py
+++ setup.py
@@ -18,7 +18,8 @@ setup(name='meta',
           'scikit-image>=0.12',
           'pyDOE2==1.3.0',
           'sentence_transformers',
-          'lime @ https://github.com/anasampa/lime/archive/vector_emb.zip',
+          #'lime @ https://github.com/anasampa/lime/archive/vector_emb.zip',
+          'lime @ https://github.com/anasampa/metaembedding/archive/master.zip',  
           'ktrain'
       ],
       extras_require={
